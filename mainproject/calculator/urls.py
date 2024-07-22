3940dfb0568e7a9b660a9b5353406349a66bd455
srujit29 <anjaneyasrujit@atai.ai>
2024-07-22 16:44:30 +0530

    deleted pyc files and updated re_path instead of urls

--- mainproject/calculator/urls.py
+++ mainproject/calculator/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from calculator import views
 
 
 urlpatterns = [
-    url(r'^$', views.HomePage.as_view()),
+    re_path(r'^$', views.HomePage.as_view()),
 ]
